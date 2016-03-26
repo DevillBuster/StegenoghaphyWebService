71ae26c5c74d1a2a89319b720e445602642b140b
Moazam Iftikhar <Moazam Iftikhar>
2016-03-26 10:03:32 +0500

    1st ty

---
+++ webservice/webservice/WebService1.asmx.cs
@@ -0,0 +1,539 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Drawing;
+using System.IO;
+
+namespace webservice
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://abc.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Steganography : System.Web.Services.WebService
+    {
+        int height, width;
+        [WebMethod]
+        public byte[] DecryptLayer(Bitmap DecryptedBitmap)
+        {
+
+
+          //  DecryptedImage = img;
+            height = DecryptedBitmap.Height;
+            width = DecryptedBitmap.Width;
+            //DecryptedBitmap = new Bitmap(DecryptedImage);
+
+
+
+            int i, j = 0;
+            bool[] t = new bool[8];
+            bool[] rb = new bool[8];
+            bool[] gb = new bool[8];
+            bool[] bb = new bool[8];
+            Color pixel = new Color();
+            byte r, g, b;
+            pixel = DecryptedBitmap.GetPixel(width - 1, height - 1);
+            long fSize = pixel.R + pixel.G * 100 + pixel.B * 10000;
+            pixel = DecryptedBitmap.GetPixel(width - 2, height - 1);
+            long fNameSize = pixel.R + pixel.G * 100 + pixel.B * 10000;
+            byte[] res = new byte[fSize];
+            string resFName = "";
+            byte temp;
+
+            //Read file name:
+            for (i = 0; i < height && i * (height / 3) < fNameSize; i++)
+                for (j = 0; j < (width / 3) * 3 && i * (height / 3) + (j / 3) < fNameSize; j++)
+                {
+                    pixel = DecryptedBitmap.GetPixel(j, i);
+                    r = pixel.R;
+                    g = pixel.G;
+                    b = pixel.B;
+                    byte2bool(r, ref rb);
+                    byte2bool(g, ref gb);
+                    byte2bool(b, ref bb);
+                    if (j % 3 == 0)
+                    {
+                        t[0] = rb[7];
+                        t[1] = gb[7];
+                        t[2] = bb[7];
+                    }
+                    else if (j % 3 == 1)
+                    {
+                        t[3] = rb[7];
+                        t[4] = gb[7];
+                        t[5] = bb[7];
+                    }
+                    else
+                    {
+                        t[6] = rb[7];
+                        t[7] = gb[7];
+                        temp = bool2byte(t);
+                        resFName += (char)temp;
+                    }
+                }
+
+            //Read file on layer 8 (after file name):
+            int tempj = j;
+            i--;
+
+            for (; i < height && i * (height / 3) < fSize + fNameSize; i++)
+                for (j = 0; j < (width / 3) * 3 && i * (height / 3) + (j / 3) < (height * (width / 3) * 3) / 3 - 1 && i * (height / 3) + (j / 3) < fSize + fNameSize; j++)
+                {
+                    if (tempj != 0)
+                    {
+                        j = tempj;
+                        tempj = 0;
+                    }
+                    pixel = DecryptedBitmap.GetPixel(j, i);
+                    r = pixel.R;
+                    g = pixel.G;
+                    b = pixel.B;
+                    byte2bool(r, ref rb);
+                    byte2bool(g, ref gb);
+                    byte2bool(b, ref bb);
+                    if (j % 3 == 0)
+                    {
+                        t[0] = rb[7];
+                        t[1] = gb[7];
+                        t[2] = bb[7];
+                    }
+                    else if (j % 3 == 1)
+                    {
+                        t[3] = rb[7];
+                        t[4] = gb[7];
+                        t[5] = bb[7];
+                    }
+                    else
+                    {
+                        t[6] = rb[7];
+                        t[7] = gb[7];
+                        temp = bool2byte(t);
+                        res[i * (height / 3) + j / 3 - fNameSize] = temp;
+                    }
+                }
+
+            //Read file on other layers:
+            long readedOnL8 = (height * (width / 3) * 3) / 3 - fNameSize - 1;
+
+            for (int layer = 6; layer >= 0 && readedOnL8 + (6 - layer) * ((height * (width / 3) * 3) / 3 - 1) < fSize; layer--)
+                for (i = 0; i < height && i * (height / 3) + readedOnL8 + (6 - layer) * ((height * (width / 3) * 3) / 3 - 1) < fSize; i++)
+                    for (j = 0; j < (width / 3) * 3 && i * (height / 3) + (j / 3) + readedOnL8 + (6 - layer) * ((height * (width / 3) * 3) / 3 - 1) < fSize; j++)
+                    {
+                        pixel = DecryptedBitmap.GetPixel(j, i);
+                        r = pixel.R;
+                        g = pixel.G;
+                        b = pixel.B;
+                        byte2bool(r, ref rb);
+                        byte2bool(g, ref gb);
+                        byte2bool(b, ref bb);
+                        if (j % 3 == 0)
+                        {
+                            t[0] = rb[layer];
+                            t[1] = gb[layer];
+                            t[2] = bb[layer];
+                        }
+                        else if (j % 3 == 1)
+                        {
+                            t[3] = rb[layer];
+                            t[4] = gb[layer];
+                            t[5] = bb[layer];
+                        }
+                        else
+                        {
+                            t[6] = rb[layer];
+                            t[7] = gb[layer];
+                            temp = bool2byte(t);
+                            res[i * (height / 3) + j / 3 + (6 - layer) * ((height * (width / 3) * 3) / 3 - 1) + readedOnL8] = temp;
+                        }
+                    }
+
+            return res;
+        }
+
+
+        private byte bool2byte(bool[] inp)
+        {
+            byte outp = 0;
+            for (short i = 7; i >= 0; i--)
+            {
+                if (inp[i])
+                    outp += (byte)Math.Pow(2.0, (double)(7 - i));
+            }
+            return outp;
+        }
+
+        private void byte2bool(byte inp, ref bool[] outp)
+        {
+            if (inp >= 0 && inp <= 255)
+                for (short i = 7; i >= 0; i--)
+                {
+                    if (inp % 2 == 1)
+                        outp[i] = true;
+                    else
+                        outp[i] = false;
+                    inp /= 2;
+                }
+            else
+                throw new Exception("Input number is illegal.");
+        }
+
+
+        /// <summary>
+        /// ////////////////////////////////////////
+        /// </summary>
+        /// <param name="bmp"></param>
+        /// <returns></returns>
+        /// 
+        [WebMethod]
+        public string extractText(Bitmap bmp)
+        {
+            int colorUnitIndex = 0;
+            int charValue = 0;
+
+            string extractedText = String.Empty;
+
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+
+                    for (int n = 0; n < 3; n++)
+                    {
+                        switch (colorUnitIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    charValue = charValue * 2 + pixel.R % 2;
+                                } break;
+                            case 1:
+                                {
+                                    charValue = charValue * 2 + pixel.G % 2;
+                                } break;
+                            case 2:
+                                {
+                                    charValue = charValue * 2 + pixel.B % 2;
+                                } break;
+                        }
+
+                        colorUnitIndex++;
+
+                        if (colorUnitIndex % 8 == 0)
+                        {
+                            charValue = reverseBits(charValue);
+
+                            if (charValue == 0)
+                            {
+                                return extractedText;
+                            }
+
+                            char c = (char)charValue;
+
+                            extractedText += c.ToString();
+                        }
+                    }
+                }
+            }
+
+            return extractedText;
+        }
+
+        public static int reverseBits(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+
+                n /= 2;
+            }
+
+            return result;
+        }
+    ///////////////////////////////////////////////////////////////////
+        //Encryption methods
+
+
+
+        long fileSize, fileNameSize;
+      //  Image loadedTrueImage, AfterEncryption;
+
+       byte[] fileContainer;
+
+        [WebMethod]
+        public void EncryptLayer(Bitmap loadedTrueBitmap , long finfo , int fileNameSize, byte[] fileContainer)
+        {
+            
+                height = loadedTrueBitmap.Height;
+                width = loadedTrueBitmap.Width;
+               // loadedTrueBitmap = new Bitmap(loadedTrueImage);
+                fileSize = finfo;
+                this.fileNameSize = fileNameSize;
+                this.fileContainer = fileContainer;
+                long FSize = fileSize;
+                Bitmap changedBitmap = EncryptLayer(8, loadedTrueBitmap, 0, (height * (width / 3) * 3) / 3 - fileNameSize - 1, true);
+                FSize -= (height * (width / 3) * 3) / 3 - fileNameSize - 1;
+                if (FSize > 0)
+                {
+                    for (int i = 7; i >= 0 && FSize > 0; i--)
+                    {
+                        changedBitmap = EncryptLayer(i, changedBitmap, (((8 - i) * height * (width / 3) * 3) / 3 - fileNameSize - (8 - i)), (((9 - i) * height * (width / 3) * 3) / 3 - fileNameSize - (9 - i)), false);
+                        FSize -= (height * (width / 3) * 3) / 3 - 1;
+                    }
+                }
+               
+            }
+
+
+
+        private Bitmap EncryptLayer(int layer, Bitmap inputBitmap, long startPosition, long endPosition, bool writeFileName)
+        {
+            Bitmap outputBitmap = inputBitmap;
+            layer--;
+            int i = 0, j = 0;
+            long FNSize = 0;
+            bool[] t = new bool[8];
+            bool[] rb = new bool[8];
+            bool[] gb = new bool[8];
+            bool[] bb = new bool[8];
+            Color pixel = new Color();
+            byte r, g, b;
+
+            if (writeFileName)
+            {
+                FNSize = fileNameSize;
+
+                //write fileName:
+                for (i = 0; i < height && i * (height / 3) < fileNameSize; i++)
+                    for (j = 0; j < (width / 3) * 3 && i * (height / 3) + (j / 3) < fileNameSize; j++)
+                    {
+                        
+                        pixel = inputBitmap.GetPixel(j, i);
+                        r = pixel.R;
+                        g = pixel.G;
+                        b = pixel.B;
+                        byte2bool(r, ref rb);
+                        byte2bool(g, ref gb);
+                        byte2bool(b, ref bb);
+                        if (j % 3 == 0)
+                        {
+                            rb[7] = t[0];
+                            gb[7] = t[1];
+                            bb[7] = t[2];
+                        }
+                        else if (j % 3 == 1)
+                        {
+                            rb[7] = t[3];
+                            gb[7] = t[4];
+                            bb[7] = t[5];
+                        }
+                        else
+                        {
+                            rb[7] = t[6];
+                            gb[7] = t[7];
+                        }
+                        Color result = Color.FromArgb((int)bool2byte(rb), (int)bool2byte(gb), (int)bool2byte(bb));
+                        outputBitmap.SetPixel(j, i, result);
+                    }
+                i--;
+            }
+            //write file (after file name):
+            int tempj = j;
+
+            for (; i < height && i * (height / 3) < endPosition - startPosition + FNSize && startPosition + i * (height / 3) < fileSize + FNSize; i++)
+                for (j = 0; j < (width / 3) * 3 && i * (height / 3) + (j / 3) < endPosition - startPosition + FNSize && startPosition + i * (height / 3) + (j / 3) < fileSize + FNSize; j++)
+                {
+                    if (tempj != 0)
+                    {
+                        j = tempj;
+                        tempj = 0;
+                    }
+                    byte2bool((byte)fileContainer[startPosition + i * (height / 3) + j / 3 - FNSize], ref t);
+                    pixel = inputBitmap.GetPixel(j, i);
+                    r = pixel.R;
+                    g = pixel.G;
+                    b = pixel.B;
+                    byte2bool(r, ref rb);
+                    byte2bool(g, ref gb);
+                    byte2bool(b, ref bb);
+                    if (j % 3 == 0)
+                    {
+                        rb[layer] = t[0];
+                        gb[layer] = t[1];
+                        bb[layer] = t[2];
+                    }
+                    else if (j % 3 == 1)
+                    {
+                        rb[layer] = t[3];
+                        gb[layer] = t[4];
+                        bb[layer] = t[5];
+                    }
+                    else
+                    {
+                        rb[layer] = t[6];
+                        gb[layer] = t[7];
+                    }
+                    Color result = Color.FromArgb((int)bool2byte(rb), (int)bool2byte(gb), (int)bool2byte(bb));
+                    outputBitmap.SetPixel(j, i, result);
+
+                }
+            long tempFS = fileSize, tempFNS = fileNameSize;
+            r = (byte)(tempFS % 100);
+            tempFS /= 100;
+            g = (byte)(tempFS % 100);
+            tempFS /= 100;
+            b = (byte)(tempFS % 100);
+            Color flenColor = Color.FromArgb(r, g, b);
+            outputBitmap.SetPixel(width - 1, height - 1, flenColor);
+
+            r = (byte)(tempFNS % 100);
+            tempFNS /= 100;
+            g = (byte)(tempFNS % 100);
+            tempFNS /= 100;
+            b = (byte)(tempFNS % 100);
+            Color fnlenColor = Color.FromArgb(r, g, b);
+            outputBitmap.SetPixel(width - 2, height - 1, fnlenColor);
+
+            return outputBitmap;
+        }
+        
+        private string justFName(string path)
+        {
+            string output;
+            int i;
+            if (path.Length == 3)   // i.e: "C:\\"
+                return path.Substring(0, 1);
+            for (i = path.Length - 1; i > 0; i--)
+                if (path[i] == '\\')
+                    break;
+            output = path.Substring(i + 1);
+            return output;
+        }
+
+        enum State
+        {
+            hiding,
+            filling_with_zeros
+        };
+
+
+
+        /// <summary>
+        /// //////////
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="bmp"></param>
+        /// <returns></returns>
+        /// 
+        [WebMethod]
+        public Bitmap embedText(string text, Bitmap bmp)
+        {
+            State s = State.hiding;
+
+            int charIndex = 0;
+            int charValue = 0;
+            long colorUnitIndex = 0;
+
+            int zeros = 0;
+
+            int R = 0, G = 0, B = 0;
+
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+
+                    pixel = Color.FromArgb(pixel.R - pixel.R % 2,
+                        pixel.G - pixel.G % 2, pixel.B - pixel.B % 2);
+
+                    R = pixel.R; G = pixel.G; B = pixel.B;
+
+                    for (int n = 0; n < 3; n++)
+                    {
+                        if (colorUnitIndex % 8 == 0)
+                        {
+                            if (zeros == 8)
+                            {
+                                if ((colorUnitIndex - 1) % 3 < 2)
+                                {
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+
+                                return bmp;
+                            }
+
+                            if (charIndex >= text.Length)
+                            {
+                                s = State.filling_with_zeros;
+                            }
+                            else
+                            {
+                                charValue = text[charIndex++];
+                            }
+                        }
+
+                        switch (colorUnitIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    if (s == State.hiding)
+                                    {
+                                        R += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+                                } break;
+                            case 1:
+                                {
+                                    if (s == State.hiding)
+                                    {
+                                        G += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+                                } break;
+                            case 2:
+                                {
+                                    if (s == State.hiding)
+                                    {
+                                        B += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                } break;
+                        }
+
+                        colorUnitIndex++;
+
+                        if (s == State.filling_with_zeros)
+                        {
+                            zeros++;
+                        }
+                    }
+                }
+            }
+
+            return bmp;
+        }
+
+       
+
+
+
+
+
+    
+    
+    
+    }
+}
